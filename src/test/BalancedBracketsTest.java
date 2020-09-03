ace5ab81b1177371cc806f7c3a359a13a7fa4050
Kavitha <anikavi73@gmail.com>
2020-09-02 20:00:36 -0500

    Testing code ready

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -12,5 +13,64 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void leftBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void rightBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+
+    @Test
+    public void stringInBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
+
+    @Test
+    public void reversedBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void InBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("LaunchCode[]"));
+    }
+
+    @Test
+    public void stringWrongInBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]LaunchCode["));
+    }
+
+    @Test
+    public void stringWrongRtInBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]LaunchCode]"));
+    }
+
+    @Test
+    public void stringWrongDoubleRtInBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]]LaunchCode]"));
+    }
+
+    @Test
+    public void stringWrongDoubleInBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode]]"));
+    }
+
+    @Test
+    public void stringWrongExtraRtInBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode[]"));
+    }
+
+    @Test
+    public void stringWrongBKInBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]Launch[Code]]"));
+    }
 
 }
